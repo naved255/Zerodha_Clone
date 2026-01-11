9ad0eca3df6c98ec9ac6cc0bd5cf9c405db7619a
naved255 <navedahmad2314lko@gamil.com>
2026-01-11 17:04:22 +0530

    url added

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,7 @@ let app = express();
 const conn = mongoose.connect(connString).then(() => { console.log("db connected") });
 
 app.use(cors({
-  origin: ["http://localhost:5174", "http://localhost:5173"],
+  origin: ["http://localhost:5174", "http://localhost:5173","https://dashboard-pi-khaki-74.vercel.app/"],
   credentials: true
 }));
 
